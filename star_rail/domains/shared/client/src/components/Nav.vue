473cd54b08fb0568ee1586719d9fe03e93c44cb1
Joraffe <jordanwmedina@gmail.com>
2023-07-30 13:27:23 -0700

    [nav] Added a nav bar + removed some of the boilerplate css

---
+++ star_rail/domains/shared/client/src/components/Nav.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <!-- This is following the materialize.css navbar template -->
+  <nav>
+    <div class="nav-wrapper teal lighten-1">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><RouterLink to="/">Home</RouterLink></li>
+        <li><RouterLink to="/about">About</RouterLink></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+</style>
