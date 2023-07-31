fae244dc790fae80bb0e1726feed404cc03fe79b
Joraffe <jordanwmedina@gmail.com>
2023-07-31 01:07:08 -0700

    [home] Update HomeView with Jobine greeting

---
+++ domains/shared/client/src/components/LandingItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="col s6">
+    <slot name="image"></slot>
+  </div>
+  <div class="col s6">
+    <div class="blurb-wrapper valign-wrapper">
+      <slot name="blurb"></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.blurb-wrapper {
+  height: 500px;
+}
+</style>
---
+++ domains/shared/client/src/components/blurbs/BlurbJobine.vue
@@ -0,0 +1,3 @@
+<template>
+  <p>Heddo cae, this is Jobine</p>
+</template>
